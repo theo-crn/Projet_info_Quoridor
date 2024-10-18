b08c398fb9faa3c1a00c62de86b0b522c4489e30
theo-crn <theophile.carayon@edu.ece.fr>
2024-10-18 12:32:05 +0200

    sousprog

---
+++ sous_prog.c
@@ -0,0 +1,5 @@
+//
+// Created by theop on 18/10/2024.
+//
+
+#include "sous_prog.h"
---
+++ sous_prog.h
@@ -0,0 +1,8 @@
+//
+// Created by theop on 18/10/2024.
+//
+
+#ifndef SOUS_PROG_H
+#define SOUS_PROG_H
+
+#endif //SOUS_PROG_H

bd27995c6f30522c7b3813999266e0784667092a
eloi-kgg <eloi.gaillard@edu.ece.fr>
2024-10-31 20:14:26 +0100

    pseudo
    ecrire et enregistrer les pseudos des joueurs

---
+++ pseudo.h
@@ -0,0 +1,11 @@
+//
+// Created by Gabriel on 31/10/2024.
+//
+
+#ifndef PSEUDO_H
+#define PSEUDO_H
+#define nombre_joueurs_max 4
+#define longueur_pseduo_max 20
+
+void pseudo (char pseudo [][longueur_pseduo_max], int nombre_joueurs_max);
+#endif //PSEUDO_H

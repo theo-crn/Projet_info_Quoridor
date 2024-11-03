418f52e60ca02164741e032a08ea8adc29b6b512
Adrien-Botin <adrien.botin@edu.ece.fr>
2024-11-03 17:00:37 +0100

    afficher regles
    fonction qui affiche les regles

---
+++ cmake-build-debug/CMakeFiles/Projet_info_Quoridor.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_info_Quoridor.dir/main.c.obj"
+  "CMakeFiles/Projet_info_Quoridor.dir/main.c.obj.d"
+  "CMakeFiles/Projet_info_Quoridor.dir/sous_prog.c.obj"
+  "CMakeFiles/Projet_info_Quoridor.dir/sous_prog.c.obj.d"
+  "Projet_info_Quoridor.exe"
+  "Projet_info_Quoridor.exe.manifest"
+  "Projet_info_Quoridor.pdb"
+  "libProjet_info_Quoridor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_info_Quoridor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Projet_info_Quoridor.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Projet_info_Quoridor.
+# This may be replaced when dependencies are built.
